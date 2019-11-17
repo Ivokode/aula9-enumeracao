d4331341d5542253bd24b1a17c323feebf694c50
Ivan Guilherme <ivan_guilherme@hotmail.com.br>
2019-11-17 03:13:42 -0300

    Abertura de Aula Enumerações

---
+++ aula9-enumeracao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aula9_enumeracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
